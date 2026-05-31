4bd260c5d4d65d76f3225c66c2cf09a58db990a6
huagru <hugoagustinruiz@gmail.com>
2026-05-31 17:56:42 -0300

    terminado el parser de erchivo txt a arreglo

--- src/parser.js
+++ src/parser.js
@@ -1,33 +1,35 @@
-// importa el modulo fs de node, para trabajar con archivos.
+// The objective of this parser is to extract IP addresses from Apache log files, to an object.
+// The object is exported for use in other modules.
+
+
+// Import the Node.js fs module to work with files.
 const fs = require("fs");
 
-// Importa el modulo path de node, para trabajar con rutas de archivos.
+// Import the Node.js path module to work with file paths.
 const path = require("path");
 
 
-
-// Define la ruta del archivo de log utilizando el módulo 'path'.
-const ruta = path.join(__dirname, "..", "logs", "log.txt");
-
-
-
-
-function parsearApache(ruta) {
-    // Lee el texto del archivo y lo almacena como un string único en 'contenido'.
-    const contenido = fs.readFileSync(ruta, "utf-8");
-
-    // Divide el contenido del archivo en líneas ("\n"). Cada línea es un elemento de registros. 
-    const registros = contenido.split("\n");
+// Define the log file path using the path module.
+const logFilePath = path.join(
+    __dirname,
+    "..",
+    "logs",
+    "log.txt"
+);
 
 
-    
-    console.log(registros);
+/* Function to parse the log file into an array */
+function parseToArray(filePath) {
+    // Read the file contents and store them as a single string.
+    const content = fs.readFileSync(filePath, "utf8");
 
-    
+    // Split the file contents into lines. Each line becomes an element in the "records" array.
+    const records = content.split("\n");
 
+    console.log(records)
 
 }
 
 
-parsearApache(ruta);
+parseToArray(logFilePath);
 
