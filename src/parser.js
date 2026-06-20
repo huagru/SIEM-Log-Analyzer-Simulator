68b0d67e7c3a285742f684bac7db22edea669ad6
huagru <hugoagustinruiz@gmail.com>
2026-06-20 19:20:00 -0300

    terminado el parser

--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,5 @@
 // The objective of this parser is to extract IP addresses from Apache log files, to an object.
-// The object is exported for use in other modules.
+// The object then is exported to be used by the analyzer.
 
 
 // Import the Node.js fs module to work with files.
@@ -7,7 +7,6 @@ const fs = require("fs");
 
 // Import the Node.js path module to work with file paths.
 const path = require("path");
-
 
 // Define the log file path using the path module.
 const logFilePath = path.join(
@@ -31,21 +30,35 @@ function parseToArray(filePath) {
 }
 
 
+// Here the function is called to parse the log file into an array. Each element is a line from the log file.
 const recordsArray = parseToArray(logFilePath);
 
 
-
+// Function to parse the array into an object
 function parseArrayToObject(array) {
     let events = [];
+    
+    // This regex line separates the different parts of the log entry.
+    const regex = /APACHE\s+(\d+\.\d+\.\d+\.\d+)\s+-\s+-\s+\[([^\]]+)\]\s+"(\w+)\s+([^"]+?)\s+(HTTP\/[\d.]+)"\s+(\d+)\s+(\d+)\s+"[^"]*"\s+"([^"]*)"/;
+
 
     for (const record of array) {
-        const match = record.match(/\d+\.\d+\.\d+\.\d+/);
+        const match = record.match(regex);
         
         if (!match) {
             continue;
         }
         
-        events.push({ ip: match[0] });
+        events.push({ 
+            ip: match[1],
+            timestamp: match[2],
+            method: match[3],
+            resource: match[4],
+            protocol: match[5],
+            status: Number(match[6]),
+            bytes: Number(match[7]),
+            userAgent: match[8]
+         });
     }
     
     console.log(events);
@@ -53,3 +66,4 @@ function parseArrayToObject(array) {
 }
 
 parseArrayToObject(recordsArray);
+
